994dcd12f87b167e69d67c9b8225f305d5c0cd4c
hamayokokuririn <k.saitoh0526@gmail.com>
2020-05-03 15:08:08 +0900

    hello

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def another_function(x, y):
+    return x + y
+
+
+def main():
+    print('Hello world')
+    result = another_function(1, 2)
+    print(result)
+
+
+
+if __name__ == '__main__':
+    main()
